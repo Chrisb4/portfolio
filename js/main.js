97e8c8b3c27b8ab506142be12f742a68bfba7c89
Christopher Brown <nudgechris@gmail.com>
2017-06-27 17:17:47 -0500

    add blinking cursor

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-const $ = function() {
-    return "Maybe you don't need JQuery..."
+var headers = document.getElementsByTagName('h1');
+headers[0].classList.add('blink')
+var didScroll = false;
+window.onscroll = scrollCheck;
+function scrollCheck() {
+  didScroll = true;
 }
-console.log("Maybe you don't need JS???  ¯\_(ツ)_/¯");
+function setBlink(){
+  var y = window.scrollY;
+  if (y < 125) {
+    headers[0].classList.add('blink');
+    headers[1].classList.remove('blink');
+    headers[2].classList.remove('blink');
+  } else if (y > 125 && y < 450) {
+    headers[0].classList.remove('blink');
+    headers[1].classList.add('blink');
+    headers[2].classList.remove('blink');
+  } else if (y > 450) {
+    headers[0].classList.remove('blink');
+    headers[1].classList.remove('blink');
+    headers[2].classList.add('blink');
+  }
+}
+setInterval(function() {
+  if(didScroll) {
+    didScroll = false;
+    setBlink();
+  }
+}, 100);
